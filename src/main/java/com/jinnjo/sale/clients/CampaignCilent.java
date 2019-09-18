6c83ba3b49f339dc42595597f06026a535c05694
fengshuai <36511574@qq.com>
2019-09-18 16:24:11 +0800

    no msg

--- src/main/java/com/jinnjo/sale/clients/CampaignCilent.java
+++ src/main/java/com/jinnjo/sale/clients/CampaignCilent.java
@@ -5,11 +5,9 @@ import com.jinnjo.sale.domain.vo.MarketingCampaignVo;
 import com.jinnjo.sale.domain.vo.PageVo;
 import com.jinnjo.sale.domain.vo.SeckillGoodsVo;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 
@@ -45,10 +43,10 @@ public interface CampaignCilent {
     PageVo<MarketingCampaignVo> getSeckillByPage(@RequestParam("startSeckillTime")String startSeckillTime, @RequestParam("endSeckillTime")String endSeckillTime, @RequestParam("page") Integer page, @RequestParam("size") Integer size, @RequestParam("status") Integer status);
 
     @GetMapping(value = "/campaigns/goodsid", headers = {FeignSecurityBean.SECURITY_AUTH_SERVICE})
-    MarketingCampaignVo getCampaignsByGoodsId(@RequestParam("data") String date, @RequestParam("goodsId") Long goodsId);
+    MarketingCampaignVo getCampaignsByGoodsId(@RequestParam("date") String date, @RequestParam("goodsId") Long goodsId);
 
     @GetMapping(value = "/campaigns/checkseckillgoods", headers = {FeignSecurityBean.SECURITY_AUTH_SERVICE})
-    Long checkSeckillGoods(@RequestParam("data") String date, @RequestParam("goodsId") Long goodsId, @RequestParam("goodsSpecId") Long goodsSpecId);
+    Long checkSeckillGoods(@RequestParam("date") String date, @RequestParam("goodsId") Long goodsId, @RequestParam("goodsSpecId") Long goodsSpecId);
 
     @PostMapping(value = "/campaigns/checkseckilltime", headers = {FeignSecurityBean.SECURITY_AUTH_SERVICE})
     Long checkSeckillTime(@RequestParam("startSeckillTime") String startSeckillTime, @RequestParam("endSeckillTime") String endSeckillTime);
